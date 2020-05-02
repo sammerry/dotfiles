9ad28f5d1a1d96ce817fe2afda5ded4af79535f4
Sam Merry <sam.merry@gmail.com>
2020-05-02 00:10:50 -0700

    fish term

---
+++ .config/fish/conf.d/00-env.fish
@@ -0,0 +1,11 @@
+# rust bin
+set -x PATH $HOME/.cargo/bin $PATH
+
+# ruby bin
+set -x PATH $HOME/.rvm/bin $PATH
+
+# Add system bin
+set -x PATH /usr/local/bin $PATH
+set -x PATH /usr/local/sbin $PATH
+set -x PATH $HOME/.local/bin $PATH
+
